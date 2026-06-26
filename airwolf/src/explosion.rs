3b49ef4bb80e2f91798080ebde809343c0bfe149
Zeeshan Ali Khan <zeenix@gmail.com>
2026-06-26 18:46:23 +0200

    Make Explosion lighter

--- airwolf/src/explosion.rs
+++ airwolf/src/explosion.rs
@@ -16,10 +16,10 @@ impl Explosion {
             sparks
                 .push(Spark {
                     pos: pos.clone(),
-                    x_velocity: ctx.random(-1.0..1.0),
-                    y_velocity: ctx.random(-1.0..1.0),
-                    radius: ctx.random(0.5..1.0),
-                    mass: ctx.random(0.5..2.0),
+                    x_velocity: ctx.random_integer(MIN_VELOCIY..MAX_VELOCIY) as i8,
+                    y_velocity: ctx.random_integer(MIN_VELOCIY..MAX_VELOCIY) as i8,
+                    radius: ctx.random_integer(MIN_RADIUS..MAX_RADIUS) as u8,
+                    mass: ctx.random_integer(MIN_MASS..MAX_MASS) as u8,
                 })
                 // We add exactly `MAX_SPARKS` sparks so this can't be `Err`.
                 .unwrap();
@@ -41,11 +41,15 @@ impl Explosion {
                  y_velocity,
                  mass,
              }| {
-                *x += *x_velocity / *mass;
-                *y += *y_velocity / *mass;
-                *radius -= RADIUS_SHRINK_SPEED;
+                let x_velocity = *x_velocity as f32 / i8::MAX as f32;
+                let y_velocity = *y_velocity as f32 / i8::MAX as f32;
+                let mass = *mass as f32 * 8.0 / u8::MAX as f32;
 
-                *radius > 0.0
+                *x += x_velocity / mass;
+                *y += y_velocity / mass;
+                *radius = radius.saturating_sub(RADIUS_SHRINK_SPEED);
+
+                *radius > 0
             },
         );
     }
@@ -57,7 +61,8 @@ impl Explosion {
                  radius,
                  ..
              }| {
-                gfx.circle_fill(*x, *y, *radius, SPARK_COLOR);
+                let radius = *radius as f32 / u8::MAX as f32 * 2.0;
+                gfx.circle_fill(*x, *y, radius, SPARK_COLOR);
             },
         );
     }
@@ -70,12 +75,19 @@ impl Explosion {
 #[derive(Debug)]
 struct Spark {
     pos: Position,
-    x_velocity: f32,
-    y_velocity: f32,
-    radius: f32,
-    mass: f32,
+    x_velocity: i8,
+    y_velocity: i8,
+    radius: u8,
+    mass: u8,
 }
 
 const MAX_SPARKS: usize = 50;
-const RADIUS_SHRINK_SPEED: f32 = 0.1;
+const RADIUS_SHRINK_SPEED: u8 = u8::MAX / 15;
 const SPARK_COLOR: Color = Color::LIGHT_GREY;
+
+const MIN_VELOCIY: i32 = i8::MIN as i32;
+const MAX_VELOCIY: i32 = i8::MAX as i32;
+const MIN_RADIUS: i32 = (u8::MAX / 15) as i32;
+const MAX_RADIUS: i32 = u8::MAX as i32;
+const MIN_MASS: i32 = (u8::MAX / 2) as i32;
+const MAX_MASS: i32 = u8::MAX as i32;
